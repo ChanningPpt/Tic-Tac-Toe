859296b8f83d7b61e4e084c82c3539c8083f23b3
Channing Ppt <channing.phong@gmail.com>
2024-10-15 03:20:28 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+
+    def display_board(self):
+        return
+
+    def make_move(self):
+        pass
+    
+    def check_winner(self):
+        pass
+
+    def is_
+
+class Player:
+    def __init__(self):
+        super().__init__()
+
+class Human(Player):
+    def __init__(self):
+        super().__init__()
+
+class Computer(Player):
+    def __init__(self):
+        super().__init__()
